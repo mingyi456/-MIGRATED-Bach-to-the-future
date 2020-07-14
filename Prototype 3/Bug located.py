eceb07bcae187840555a0781ee5cd99b5b95fef7
mingyi456 <mingyi456@hotmail.com>
2020-07-14 14:53:45 +0800

    Cleaned up code isolating bug

--- Prototype 3/Bug located.py
+++ Prototype 3/Bug located.py
@@ -1,35 +1,21 @@
 from os import listdir
 import vlc
 from sys import exit
-
+from time import sleep
 import pygame
-
-class Foo:
-	def __init__(self):
-		self.wav_files= []
-		for i in listdir(".\\wav_files\\"):
-			self.wav_files.append(vlc.MediaPlayer(f".\\wav_files\\{i}"))
-
-def playall():
-	for i in Foo().wav_files:
-		i.play()
 
 
 if __name__ == "__main__":
 	
-	playall() # Here there is no error
-
-	pygame.mixer.init()
 	pygame.display.init()
-
-	screen = pygame.display.set_mode((600, 400))
+	pygame.mixer.init()
+	pygame.display.set_mode((600, 400))
 	
-	# playall() # Here there is an error
-		
+	player= vlc.MediaPlayer("./wav_files/pavane.wav")
+	player.play()
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
-
-
