78b65c20ded4fa25bac352242d93a8ad2199d2d5
YiJia Chen <chence12@gmail.com>
2020-07-15 17:36:15 +0800

    shortmidi.mid new

--- Prototype 3/fadeout.py
+++ Prototype 3/fadeout.py
@@ -2,7 +2,10 @@ self.curr_alpha += self.fade_spd
 if self.forceDone:
 	self.curr_alpha= 255
 self.mask.set_alpha(self.curr_alpha)
-self.background= self.bg_copy.copy()
-self.background.blit(self.mask, (0, 0))
+# self.background= self.bg_copy.copy()
+# self.background.blit(self.mask, (0, 0))
+self.curr_alpha = min(self.curr_alpha, 255)
+self.background.fill((0,0,0, self.curr_alpha))
 if self.curr_alpha < 255:
 	self.scriptsDone.append(False)
+print(self.curr_alpha)
