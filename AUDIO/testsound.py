c9bc396ae00e4a8fdf68d322faba685da6b148ea
YiJia Chen <chence12@gmail.com>
2020-07-16 18:56:39 +0800

    update storyline

--- AUDIO/testsound.py
+++ AUDIO/testsound.py
@@ -2,7 +2,7 @@ import pygame, os, sys
 from pygame.locals import *
 
 pygame.mixer.init()
-shootSound = pygame.mixer.Sound('playershoot.wav')
+shootSound = pygame.mixer.Sound('Sheep may safely graze.ogg')
 shootSound.play()
 while pygame.mixer.get_busy():
     pass
